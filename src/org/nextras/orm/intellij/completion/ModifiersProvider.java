7325d1dab63a4deeb60b784e4ef2bf21eb20b64f
hrach <hrach.cz@gmail.com>
2015-08-16 10:31:00 +0200

    modifiers: added completion for modifier names

--- src/org/nextras/orm/intellij/completion/ModifiersProvider.java
+++ src/org/nextras/orm/intellij/completion/ModifiersProvider.java
@@ -1,8 +1,7 @@
 package org.nextras.orm.intellij.completion;
 
-import com.intellij.codeInsight.completion.CompletionParameters;
-import com.intellij.codeInsight.completion.CompletionProvider;
-import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.php.PhpIcons;
@@ -11,12 +10,24 @@ import org.jetbrains.annotations.NotNull;
 
 public class ModifiersProvider extends CompletionProvider<CompletionParameters>
 {
+	public static ModifierInsertHandler withParams = new ModifierInsertHandler(true);
+	public static ModifierInsertHandler withoutParams = new ModifierInsertHandler(false);
+
 	@Override
 	protected void addCompletions(@NotNull CompletionParameters params,
 	                              ProcessingContext context,
 	                              @NotNull CompletionResultSet result)
 	{
-		LookupElementBuilder el = LookupElementBuilder.create("default").withIcon(PhpIcons.FIELD).withTypeText("default", true);
-		result.addElement(el);
+		result.addElement(LookupElementBuilder.create("enum").withInsertHandler(withParams));
+		result.addElement(LookupElementBuilder.create("default").withInsertHandler(withParams));
+		result.addElement(LookupElementBuilder.create("virtual").withInsertHandler(withoutParams));
+		result.addElement(LookupElementBuilder.create("container").withInsertHandler(withParams));
+		result.addElement(LookupElementBuilder.create("1:m").withInsertHandler(withParams));
+		result.addElement(LookupElementBuilder.create("m:1").withInsertHandler(withParams));
+		result.addElement(LookupElementBuilder.create("1:1").withInsertHandler(withParams));
+		result.addElement(LookupElementBuilder.create("1:1d").withInsertHandler(withParams));
+		result.addElement(LookupElementBuilder.create("m:n").withInsertHandler(withParams));
+		result.addElement(LookupElementBuilder.create("primary").withInsertHandler(withoutParams));
 	}
+
 }
