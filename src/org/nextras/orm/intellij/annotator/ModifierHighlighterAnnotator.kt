d9638e110351c89a297134a73cded5354e5d5e96
Jan Skrasek <hrach.cz@gmail.com>
2020-01-22 19:51:38 +0100

    highlighter: rewrite using actual parser psi elements

--- src/org/nextras/orm/intellij/annotator/ModifierHighlighterAnnotator.kt
+++ src/org/nextras/orm/intellij/annotator/ModifierHighlighterAnnotator.kt
@@ -3,22 +3,43 @@ package org.nextras.orm.intellij.annotator
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.psi.PsiElement
-import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.psi.PsiWhiteSpace
+import com.jetbrains.php.lang.documentation.phpdoc.lexer.PhpDocTokenTypes
+import com.jetbrains.php.lang.documentation.phpdoc.psi.tags.PhpDocPropertyTag
+import com.jetbrains.php.lang.psi.elements.impl.PhpPsiElementImpl
+import org.nextras.orm.intellij.annotator.highlighter.ModifierHighlighter
 
+/**
+ * This is a modifier highlighter.
+ */
 class ModifierHighlighterAnnotator : Annotator {
 	override fun annotate(psiElement: PsiElement, annotationHolder: AnnotationHolder) {
-		val parent = psiElement.parent
-		val type = (psiElement as? LeafPsiElement)?.node?.elementType
+		// currently PhpDoc property comment is always wrapped in this specific type
+		if (psiElement::class != PhpPsiElementImpl::class) return
+		if (psiElement.parent !is PhpDocPropertyTag) return
 
-		// LBRACE || RBRACE
-//		if (parent is PhpDocTagModifier && (type === PhpDocTokenTypes.DOC_LBRACE || type === PhpDocTokenTypes.DOC_RBRACE)) {
-//			annotationHolder.createInfoAnnotation(psiElement, null).textAttributes = ModifierHighlighter.BRACES
-//
-//		} else if (psiElement is PhpDocTagModifierName) {
-//			annotationHolder.createInfoAnnotation(psiElement, null).textAttributes = ModifierHighlighter.MODIFIER
-//
-//		} else if (psiElement is PhpDocTagModifierIdentifier || psiElement is PhpDocTagModifierClassType) {
-//			annotationHolder.createInfoAnnotation(psiElement, null).textAttributes = ModifierHighlighter.IDENTIFIER
-//		}
+		var isModifier = false
+		var isModifierName = false
+		var element: PsiElement? = psiElement.node.firstChildNode as? PsiElement ?: return
+
+		while (element != null) {
+			if (element.node.elementType == PhpDocTokenTypes.DOC_LBRACE) {
+				isModifier = true
+				isModifierName = true
+				annotationHolder.createInfoAnnotation(element, null).textAttributes = ModifierHighlighter.BRACES
+			} else if (element.node.elementType == PhpDocTokenTypes.DOC_RBRACE) {
+				isModifier = false
+				annotationHolder.createInfoAnnotation(element, null).textAttributes = ModifierHighlighter.BRACES
+			} else if (isModifier) {
+				if (isModifierName && element !is PsiWhiteSpace) {
+					annotationHolder.createInfoAnnotation(element, null).textAttributes = ModifierHighlighter.MODIFIER
+				} else if (!isModifierName && (element.node.elementType == PhpDocTokenTypes.DOC_IDENTIFIER || element.node.elementType == PhpDocTokenTypes.DOC_VARIABLE)) {
+					annotationHolder.createInfoAnnotation(element, null).textAttributes = ModifierHighlighter.IDENTIFIER
+				} else {
+					isModifierName = false
+				}
+			}
+			element = element.nextSibling
+		}
 	}
 }
