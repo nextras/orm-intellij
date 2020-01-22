946b75be7b349ad16f059918217df94cf95a4989
Jan Skrasek <hrach.cz@gmail.com>
2020-01-22 18:12:03 +0100

    drop custom parser for phpdoc

--- src/org/nextras/orm/intellij/annotator/ModifierHighlighterAnnotator.kt
+++ src/org/nextras/orm/intellij/annotator/ModifierHighlighterAnnotator.kt
@@ -4,12 +4,6 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
-import com.jetbrains.php.lang.documentation.phpdoc.lexer.PhpDocTokenTypes
-import org.nextras.orm.intellij.annotator.highlighter.ModifierHighlighter
-import org.nextras.orm.intellij.parser.PhpDocTagModifier
-import org.nextras.orm.intellij.parser.PhpDocTagModifierClassType
-import org.nextras.orm.intellij.parser.PhpDocTagModifierIdentifier
-import org.nextras.orm.intellij.parser.PhpDocTagModifierName
 
 class ModifierHighlighterAnnotator : Annotator {
 	override fun annotate(psiElement: PsiElement, annotationHolder: AnnotationHolder) {
@@ -17,14 +11,14 @@ class ModifierHighlighterAnnotator : Annotator {
 		val type = (psiElement as? LeafPsiElement)?.node?.elementType
 
 		// LBRACE || RBRACE
-		if (parent is PhpDocTagModifier && (type === PhpDocTokenTypes.DOC_LBRACE || type === PhpDocTokenTypes.DOC_RBRACE)) {
-			annotationHolder.createInfoAnnotation(psiElement, null).textAttributes = ModifierHighlighter.BRACES
-
-		} else if (psiElement is PhpDocTagModifierName) {
-			annotationHolder.createInfoAnnotation(psiElement, null).textAttributes = ModifierHighlighter.MODIFIER
-
-		} else if (psiElement is PhpDocTagModifierIdentifier || psiElement is PhpDocTagModifierClassType) {
-			annotationHolder.createInfoAnnotation(psiElement, null).textAttributes = ModifierHighlighter.IDENTIFIER
-		}
+//		if (parent is PhpDocTagModifier && (type === PhpDocTokenTypes.DOC_LBRACE || type === PhpDocTokenTypes.DOC_RBRACE)) {
+//			annotationHolder.createInfoAnnotation(psiElement, null).textAttributes = ModifierHighlighter.BRACES
+//
+//		} else if (psiElement is PhpDocTagModifierName) {
+//			annotationHolder.createInfoAnnotation(psiElement, null).textAttributes = ModifierHighlighter.MODIFIER
+//
+//		} else if (psiElement is PhpDocTagModifierIdentifier || psiElement is PhpDocTagModifierClassType) {
+//			annotationHolder.createInfoAnnotation(psiElement, null).textAttributes = ModifierHighlighter.IDENTIFIER
+//		}
 	}
 }
--- src/org/nextras/orm/intellij/completion/ModifiersPatterns.kt
+++ src/org/nextras/orm/intellij/completion/ModifiersPatterns.kt
@@ -5,12 +5,11 @@ import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.PsiElement
 import com.jetbrains.php.lang.PhpLanguage
 import com.jetbrains.php.lang.documentation.phpdoc.lexer.PhpDocTokenTypes
-import org.nextras.orm.intellij.parser.PhpDocTypes
 
 object ModifiersPatterns {
 	val modifierPattern: ElementPattern<PsiElement>
 		get() = PlatformPatterns.psiElement(PhpDocTokenTypes.DOC_IDENTIFIER)
 			.afterLeaf(PlatformPatterns.psiElement(PhpDocTokenTypes.DOC_LBRACE))
-			.inside(PlatformPatterns.psiElement(PhpDocTypes.phpDocTagModifier))
+//			.inside(PlatformPatterns.psiElement(PhpDocTypes.phpDocTagModifier))
 			.withLanguage(PhpLanguage.INSTANCE)
 }
