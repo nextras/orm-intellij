7b1e0fae80524120bd519178dc4aaafee73a56c9
Jan Skrasek <hrach.cz@gmail.com>
2021-05-05 21:43:38 +0200

    update to latest Kotlin & gradle & intellij plugin

--- src/org/nextras/orm/intellij/actions/generation/GenerateGettersSettersAction.kt
+++ src/org/nextras/orm/intellij/actions/generation/GenerateGettersSettersAction.kt
@@ -14,8 +14,8 @@ class GenerateGettersSettersAction : CodeInsightAction() {
 	private val actionHandler = object : GenerateActionHandler() {
 		override fun canShow(property: PhpDocProperty, phpClass: PhpClass): Boolean {
 			val name = property.name
-			val getterMethodName = "getter" + name.substring(0, 1).toUpperCase() + name.substring(1)
-			val setterMethodName = "setter" + name.substring(0, 1).toUpperCase() + name.substring(1)
+			val getterMethodName = "getter" + name.substring(0, 1).uppercase() + name.substring(1)
+			val setterMethodName = "setter" + name.substring(0, 1).uppercase() + name.substring(1)
 			val getterMethod = phpClass.findMethodByName(getterMethodName)
 			val setterMethod = phpClass.findMethodByName(setterMethodName)
 			return getterMethod == null && setterMethod == null
@@ -23,9 +23,9 @@ class GenerateGettersSettersAction : CodeInsightAction() {
 
 		override fun createAccessors(field: Field, project: Project): String {
 			val name = field.name
-			val getterMethodName = "getter" + name.substring(0, 1).toUpperCase() + name.substring(1)
+			val getterMethodName = "getter" + name.substring(0, 1).uppercase() + name.substring(1)
 			val getterMethod = PhpPsiElementFactory.createMethod(project, "protected function $getterMethodName(\$value) { return \$value; }")
-			val setterMethodName = "setter" + name.substring(0, 1).toUpperCase() + name.substring(1)
+			val setterMethodName = "setter" + name.substring(0, 1).uppercase() + name.substring(1)
 			val setterMethod = PhpPsiElementFactory.createMethod(project, "protected function $setterMethodName(\$value) { return \$value; }")
 			return setterMethod.text + '\n' + getterMethod.text
 		}
