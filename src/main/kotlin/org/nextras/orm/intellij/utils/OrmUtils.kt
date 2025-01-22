493cd8d19a0c79fc63f7b872d8b1c0327574e728
Jan Škrášek <hrach.cz@gmail.com>
2025-01-22 23:04:40 +0100

    fix property autocompletion & reference contribution in findBy() methods (#103)

--- src/main/kotlin/org/nextras/orm/intellij/utils/OrmUtils.kt
+++ src/main/kotlin/org/nextras/orm/intellij/utils/OrmUtils.kt
@@ -2,17 +2,13 @@ package org.nextras.orm.intellij.utils
 
 import com.jetbrains.php.PhpIndex
 import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocProperty
-import com.jetbrains.php.lang.psi.elements.ArrayCreationExpression
-import com.jetbrains.php.lang.psi.elements.ClassConstantReference
-import com.jetbrains.php.lang.psi.elements.GroupStatement
 import com.jetbrains.php.lang.psi.elements.MemberReference
 import com.jetbrains.php.lang.psi.elements.MethodReference
 import com.jetbrains.php.lang.psi.elements.PhpClass
-import com.jetbrains.php.lang.psi.elements.PhpReturn
 import com.jetbrains.php.lang.psi.resolve.types.PhpType
 
 object OrmUtils {
-	enum class OrmClass constructor(private val className: String) {
+	enum class OrmClass constructor(val className: String) {
 		COLLECTION("\\Nextras\\Orm\\Collection\\ICollection"),
 		MAPPER("\\Nextras\\Orm\\Mapper\\IMapper"),
 		REPOSITORY("\\Nextras\\Orm\\Repository\\IRepository"),
@@ -30,62 +26,14 @@ object OrmUtils {
 		}
 	}
 
-	fun findRepositoryEntities(repositories: Collection<PhpClass>): Collection<PhpClass> {
-		val entities = HashSet<PhpClass>(1)
-		for (repositoryClass in repositories) {
-			val entityNamesMethod = repositoryClass.findMethodByName("getEntityClassNames") ?: return emptyList()
-			if (entityNamesMethod.lastChild !is GroupStatement) {
-				continue
-			}
-			if ((entityNamesMethod.lastChild as GroupStatement).firstPsiChild !is PhpReturn) {
-				continue
-			}
-			if ((entityNamesMethod.lastChild as GroupStatement).firstPsiChild!!.firstPsiChild !is ArrayCreationExpression) {
-				continue
-			}
-
-			val arr =
-				(entityNamesMethod.lastChild as GroupStatement).firstPsiChild!!.firstPsiChild as ArrayCreationExpression?
-			val phpIndex = PhpIndex.getInstance(repositoryClass.project)
-			for (el in arr!!.children) {
-				if (el.firstChild !is ClassConstantReference) {
-					continue
-				}
-				val ref = el.firstChild as ClassConstantReference
-				if (ref.name != "class") {
-					continue
-				}
-				entities.addAll(PhpIndexUtils.getByType(ref.classReference!!.type, phpIndex))
-			}
-		}
-		return entities
-	}
-
 	fun findQueriedEntities(ref: MemberReference): Collection<PhpClass> {
-		val classReference = ref.classReference ?: return emptyList()
-		val entities = HashSet<PhpClass>()
 		val phpIndex = PhpIndex.getInstance(ref.project)
-		val classes = PhpIndexUtils.getByType(classReference.type, phpIndex)
-
-		val repositories = classes.filter { cls -> OrmClass.REPOSITORY.`is`(cls, phpIndex) }
-		if (repositories.isNotEmpty()) {
-			entities.addAll(findRepositoryEntities(repositories))
+		val completedType = phpIndex.completeType(ref.project, ref.type, null)
+		val types = completedType.typesWithParametrisedParts.mapNotNull { type ->
+			if (!type.contains(OrmClass.COLLECTION.className)) return@mapNotNull null
+			PhpType.getParametrizedParts(type).firstOrNull()
 		}
-
-		for (type in classReference.type.types) {
-			if (!type.endsWith("[]")) {
-				continue
-			}
-			val typeWithoutArray = PhpType().add(type.substring(0, type.length - 2))
-			val maybeEntities = PhpIndexUtils.getByType(typeWithoutArray, phpIndex)
-			entities.addAll(
-				maybeEntities.filter { cls ->
-					OrmClass.ENTITY.`is`(cls, phpIndex)
-				}
-			)
-		}
-
-		return entities
+		return types.flatMap { PhpIndexUtils.getByType(PhpType().add(it), phpIndex) }
 	}
 
 	fun findQueriedEntities(reference: MethodReference, cls: String?, path: Array<String>): Collection<PhpClass> {
