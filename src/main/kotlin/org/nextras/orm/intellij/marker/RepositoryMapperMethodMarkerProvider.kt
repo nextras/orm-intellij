bbd749c4ce424cf86e5ed3f2046fb23b39552776
Jan Skrasek <hrach.cz@gmail.com>
2026-01-02 19:54:14 +0100

    fix null assert crash in RepositoryMapperMethodMarkerProvider [closes #134]

--- src/main/kotlin/org/nextras/orm/intellij/marker/RepositoryMapperMethodMarkerProvider.kt
+++ src/main/kotlin/org/nextras/orm/intellij/marker/RepositoryMapperMethodMarkerProvider.kt
@@ -10,26 +10,32 @@ import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocMethod
 import org.nextras.orm.intellij.utils.OrmUtils
 
 class RepositoryMapperMethodMarkerProvider : RelatedItemLineMarkerProvider() {
-	override fun collectNavigationMarkers(element: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<*>>) {
-		if (element !is PhpDocMethod) {
-			return
-		}
+	private val baseMethods = setOf(
+		"findAll",
+		"findBy",
+		"findById",
+		"getBy",
+		"getByChecked",
+		"getById",
+		"getByIdChecked",
+	)
 
-		val containingClass = element.containingClass
+	override fun collectNavigationMarkers(
+		element: PsiElement,
+		result: MutableCollection<in RelatedItemLineMarkerInfo<*>>,
+	) {
+		if (element !is PhpDocMethod) return
+
+		val containingClass = element.containingClass ?: return
 		val index = PhpIndex.getInstance(element.project)
-		if (!OrmUtils.OrmClass.REPOSITORY.`is`(containingClass!!, index)) {
-			return
-		}
-		if (baseMethods.contains(element.name)) {
-			return
-		}
+		if (!OrmUtils.OrmClass.REPOSITORY.`is`(containingClass, index)) return
+		if (baseMethods.contains(element.name)) return
 
 		val repositoryClass = containingClass.fqn
 		val mapperClass = repositoryClass.substring(0, repositoryClass.length - 10) + "Mapper"
-		val phpIndex = PhpIndex.getInstance(element.getProject())
 
-		val methods = phpIndex.getClassesByFQN(mapperClass)
-			.filter { OrmUtils.OrmClass.MAPPER.`is`(it, phpIndex) }
+		val methods = index.getClassesByFQN(mapperClass)
+			.filter { OrmUtils.OrmClass.MAPPER.`is`(it, index) }
 			.mapNotNull { it.findMethodByName(element.name) }
 
 		result.add(
@@ -38,9 +44,5 @@ class RepositoryMapperMethodMarkerProvider : RelatedItemLineMarkerProvider() {
 				.setTooltipText("Navigate to mapper method")
 				.createLineMarkerInfo(element)
 		)
-	}
-
-	companion object {
-		private val baseMethods = hashSetOf("findAll", "findBy", "findById", "getBy", "getByChecked", "getById", "getByIdChecked")
 	}
 }
